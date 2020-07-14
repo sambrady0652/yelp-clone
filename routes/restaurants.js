8ef76003b1cdd52bbe461d4742c260865aa9edb7
StedHouston <s.houston406@gmail.com>
2020-07-14 16:38:33 -0500

    updated restaurant/reviews routes

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,6 +6,7 @@ const { check } = require('express-validator');
 // - Internal Requirements
 const { asyncHandler } = require('../utils');
 const db = require('../db/models');
+const { sequelize } = require('../db/models');
 
 // - Declarations
 const router = express.Router();
@@ -14,7 +15,7 @@ const { Restaurant, User, Review } = db;
 
 // - Routes
 
-//Renders Restaurant Profile Page 
+//Renders Restaurant Profile Page
 router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     const restaurantId = parseInt(req.params.id, 10);
     const reviews = await Review.findAll({
@@ -28,28 +29,79 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
 }));
 
 //Render New Review Form
-router.get('/:id(\\d+)/reviews/new', asyncHandler(async (req, res) => {
-    res.render('review-form', { title: "New Review" })
+router.get('/:id(\\d+)/reviews/new', csrfProtection, asyncHandler(async (req, res) => {
+    res.send({token: req.csrfToken()})
+    res.render('review-form', { title: "New Review", token: req.csrfToken() })
 }));
 
 //Submits New Review Form
-router.post('/:id(\\d+)/reviews', asyncHandler(async (req, res) => {
+router.post('/:id(\\d+)/reviews', csrfProtection, asyncHandler(async (req, res) => {
+    //let userId, rating, content, photo = req.body;
+    let userId = parseInt(req.body.userId, 10)
+    console.log("userID:   " + userId)
+    let restaurantId = parseInt(req.params.id, 10);
+    let rating = parseInt(req.body.rating, 10)
+    await Review.create({
+        userId: userId,
+        restaurantId: restaurantId,
+        content: req.body.content,
+        rating: rating,
+        photos: req.body.photo,
+        coolCount: 0,
+        funnyCount: 0,
+        usefulCount: 0
+    })
+    //postman tests
+    res.send("Success");
+
+    //res.redirect(`/${restaurantId}`)
 
 }));
 
 //Renders Edit Review Form
-router.get('/:id(\\d+)/reviews/:id(\\d+)/edit', asyncHandler(async (req, res) => {
+router.get('/:id(\\d+)/reviews/:idd(\\d+)/edit', csrfProtection, asyncHandler(async (req, res) => {
+    const reviewId = parseInt(req.params.idd, 10)
+    const review = await Review.findByPk(reviewId)
+    console.log(review)
+    let { content, rating } = review
 
+    //postman tests
+    //res.send(rating + " " + content)
+    res.render('review-form', { title: "Edit Review", rating, content, token: req.csrfToken() })
 }));
 
 //Submits Edit Review Form
-router.patch('/:id(\\d+)/reviews/:id(\\d+)', asyncHandler(async (req, res) => {
+router.patch('/:id(\\d+)/reviews/:idd(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
+    let { rating, content } = req.body;
+    rating = parseInt(rating, 10)
+    const reviewId = parseInt(req.params.idd, 10)
+    const restaurantId = parseInt(req.params.id, 10)
+    const review = await Review.findByPk(reviewId)
 
+    review.content = content;
+    review.rating = rating;
+
+    await review.save();
+
+    await sequelize.close();
+
+    //postman test
+    res.send({review})
+
+    //res.redirect(`/${restaurantId}`)
 }));
 
 //deletes a review
-router.delete('/:id(\\d+)/reviews/:id(\\d+)', asyncHandler(async (req, res) => {
+router.delete('/:id(\\d+)/reviews/:idd(\\d+)', asyncHandler(async (req, res) => {
+    const restaurantId = parseInt(req.params.id, 10)
+    const review = await Review.findByPk(req.params.idd)
 
+    await review.destroy()
+
+    await sequelize.close()
+
+
+    res.redirect(`/${restaurantId}`)
 }));
 
 
