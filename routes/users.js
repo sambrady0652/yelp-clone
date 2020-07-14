ce5b99ac74170ea41002998693f50ef4df02e1e9
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-07-14 15:18:39 -0400

    route edits

--- routes/users.js
+++ routes/users.js
@@ -8,11 +8,15 @@ const { check } = require('express-validator');
 const { asyncHandler, handleValidationErrors } = require('../utils');
 const db = require('../db/models');
 const { getUserToken, requireAuth } = require('../auth');
+const { sequelize } = require('../db/models');
 
 // - Declarations
 const { User, Review, Restaurant, userFavoriteRestaurant } = db;
 const router = express.Router();
 const csrfProtection = csurf({ cookie: true });
+
+// - Users-wide Middleware
+router.use(requireAuth);
 
 // - Validations
 const validateEmailAndPassword = [
@@ -25,21 +29,37 @@ const validateEmailAndPassword = [
         .withMessage("Please provide a password."),
     handleValidationErrors,
 ];
+const validateUserSettings = [
+    check("firstName")
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide a First Name."),
+    check("lastName")
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide a Second Name."),
+    check("city")
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide a City."),
+    check("state")
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide a State."),
+];
 
-router.use(requireAuth);
 //Routes
 //Render's Signup Form
-router.get('/signup', (req, res) => {
-    res.render('user-signup', { title: 'Sign Up - Welp' })
+router.get('/signup', csrfProtection, (req, res) => {
+    res.render('user-signup', { title: 'Sign Up - Welp', crsfToken: req.crsfToken() });
 });
 
 //Create New User(submits signup form)
-router.post('/', validateEmailAndPassword, asyncHandler(async (req, res) => {
-    const { firstName, lastName, email, password, city, state } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ firstName, lastName, email, hashedPassword, city, state });
-
-    res.redirect(`/users/${parseInt(user.id, 10)}`);
+router.post(
+    '/', 
+    validateEmailAndPassword,
+    csrfProtection, 
+    asyncHandler(async (req, res) => {
+        const { firstName, lastName, email, password, city, state } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ firstName, lastName, email, hashedPassword, city, state });
+        res.redirect(`/users/${parseInt(user.id, 10)}`);
 }));
 
 //Renders User's Profile Page
@@ -57,20 +77,48 @@ router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
 }));
 
 //Renders User's Settings Form 
-router.get('/:id(\\d+)/settings', asyncHandler(async (req, res) => {
-    const userId = parseInt(req.params.id, 10);
-    const user = await User.findByPk(userId)
-    //TODO: Add Settings features to database
-    res.render('user-settings', { title: `${user.firstName}'s Settings`, user })
-}));
+router.get(
+    '/:id(\\d+)/settings', 
+    csrfProtection, 
+    asyncHandler(async (req, res) => {
+        const userId = parseInt(req.params.id, 10);
+        const user = await User.findByPk(userId);
+        
+        //TODO: Add Settings features to database
+
+        res.render('user-settings', { 
+            title: `${user.firstName}'s Settings`, 
+            user, 
+            csrfToken: req.csrfToken() 
+        });
+    })
+);
 //Edit User's Settings (Submits )
-router.patch('/:id(\\d+)', (req, res) => {
-
-});
+router.patch(
+    '/:id(\\d+)', 
+    csrfProtection, 
+    validateUserSettings,
+    asynchHandler(async (req, res) => {
+        const { firstName, lastName, city, state } = req.body;
+        const userId = parseInt(req.params.id, 10);
+        const userToUpdate = await User.findByPk(userId);
+        const user = { firstName, lastName, city, state };
+        await userToUpdate.update(user);
+        res.redirect(`/users/${parseInt(user.id, 10)}`);
+    })
+);
 //Remove User
-router.delete('/:id(\\d+)', (req, res) => {
-
-});
+// --- Changed VERB to "POST"
+// --- no validate called
+router.post(
+    '/:id(\\d+)', 
+    csrfProtection,
+    asyncHandler(async (req, res) => {
+        const userId = parseInt(req.params.id, 10);
+        const user = await User.findByPk(userId);
+        await user.destroy();
+        res.redirect('/');
+    }));
 
 //View User's Favorite restaurants
 router.get('/:id(\\d+)/favorites', asyncHandler(async (req, res) => {
