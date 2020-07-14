ac8b3f858abf68435f5656702599cfad9ee660bb
Sam Brady <sambrady0652@gmail.com>
2020-07-14 18:52:26 -0500

    Login Functionality

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
-const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next)
 const { validationResult } = require('express-validator');
 
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next)
 const handleValidationErrors = (req, res, next) => {
     const validationErrors = validationResult(req);
 
