bccf7cbfc6f2ccfeacdacb3f4cae6143fa22fbda
StedHouston <s.houston406@gmail.com>
2020-07-19 04:18:24 -0500

    fixed last merge

--- public/js/restaurant-profile-page.js
+++ public/js/restaurant-profile-page.js
@@ -1,25 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", () => {
     const userId = localStorage.getItem("WELP_CURRENT_USER_ID");
     const writeReviewBtn = document.getElementById("review");
     const userId = localStorage.getItem("WELP_CURRENT_USER_ID")
     const restaurantIdElement = document.getElementById("restaurantId")
     const restaurantId = restaurantIdElement.innerHTML;
+    const hoursBox = document.querySelector(".restaurantSubCard__hoursBox");
+
+    const getHours = () => {
+        const randomImg = Math.floor(Math.random() * Math.floor(5));
+        const img = document.createElement('img');
+        img.setAttribute("src", `/images/hours${randomImg}.jpg`);
+        hoursBox.appendChild(img);
+    };
+
+    getHours();
 
     writeReviewBtn.addEventListener("click", (e) => {
         const restaurantId = e.target.value
         window.location.href = `/restaurants/${restaurantId}/reviews/new`
-        const hoursBox = document.querySelector(".restaurantSubCard__hoursBox");
-
-        const getHours = () => {
-          const randomImg = Math.floor(Math.random() * Math.floor(5));
-          const img = document.createElement('img');
-          img.setAttribute("src", `/images/hours${randomImg}.jpg`);
-          hoursBox.appendChild(img);
-          };
-        getHours();
-
-        writeReviewBtn.addEventListener("click", (e) => {
-          const restaurantId = e.target.value
-          console.log(restaurantId, userId);
     });
 });
