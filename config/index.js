b6c905ebf46d2e262f75a02028086a0dd7e088f5
Sam Brady <sambrady0652@gmail.com>
2020-07-19 17:54:45 -0500

    moved port variable

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     environment: process.env.NODE_ENV || 'development',
-    port: Number.parseInt(process.env.PORT, 10) || 8081,
+    // port: Number.parseInt(process.env.PORT, 10) || 8081,
     db: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
